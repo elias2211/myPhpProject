1753339426de90cdcff76fc487730322c28a31e8
elias2211 <32076201+elias2211@users.noreply.github.com>
2017-09-18 16:20:34 -0500

    Add files via upload

---
+++ freight.php
@@ -0,0 +1,26 @@
+<html>
+<body>
+<table border="0" cellpadding="3">
+<tr>
+  <td bgcolor="#cccccc" align="center">Distance</td>
+  <td bgcolor="#cccccc" align="center">Cost</td>
+</tr>
+<?php
+
+  $distance = 50;
+  
+  while($distance <= 250)
+    {
+      echo "<tr>
+            <td align=\"right\">".$distance."</td>
+            <td align=\"right\">".($distance / 10)."</td>
+            </tr> \n"; 
+            
+      $distance += 50;
+    }
+    
+    ?>
+    
+    </table>
+    </body>
+    </html>
